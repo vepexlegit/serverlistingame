3a46f9a233c3e6907f3f1b3f58b27db7c42ffc42
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-16 12:19:00 +0400

    Update ServerListInGame.java

--- src/main/java/de/vepexlegit/serverlistingame/ServerListInGame.java
+++ src/main/java/de/vepexlegit/serverlistingame/ServerListInGame.java
@@ -1,23 +1,23 @@
 package de.vepexlegit.serverlistingame;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.Side;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiIngameMenu;
 import net.minecraft.client.gui.GuiMultiplayer;
 import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(modid = ServerListInGame.MODID, version = ServerListInGame.VERSION)
 public class ServerListInGame {
     public static final String MODID = "serverlistingame";
     public static final String VERSION = "1.0";
-    
+
     @EventHandler
     public void init(FMLInitializationEvent event) {
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
