497778ac60f97789878090d2ea7015590ddb5297
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-16 23:51:13 +0400

    Update ServerListInGame.java

--- src/main/java/de/vepexlegit/serverlistingame/ServerListInGame.java
+++ src/main/java/de/vepexlegit/serverlistingame/ServerListInGame.java
@@ -10,17 +10,27 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ServerListInGame.MODID, version = ServerListInGame.VERSION)
+@Mod(modid = ServerListInGame.MODID, name = ServerListInGame.NAME, version = ServerListInGame.VERSION)
 public class ServerListInGame {
     public static final String MODID = "serverlistingame";
+    public static final String NAME = "ServerListInGame";
     public static final String VERSION = "1.0";
-    
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
         if (event.getSide() == Side.CLIENT) {
             MinecraftForge.EVENT_BUS.register(this);
         }
@@ -28,17 +38,17 @@ public class ServerListInGame {
 
     @SubscribeEvent
     public void onGuiScreenInit(GuiScreenEvent.InitGuiEvent.Post event) {
-        if (event.gui instanceof GuiIngameMenu) {
-            int x = event.gui.width / 2 - 100;
-            int y = event.gui.height / 4 + 128;
-            event.buttonList.add(new GuiButton(100, x, y, "ServerList"));
+        if (event.getGui() instanceof GuiIngameMenu) {
+            int x = event.getGui().width / 2 - 100;
+            int y = event.getGui().height / 4 + 128;
+            event.getButtonList().add(new GuiButton(100, x, y, "ServerList"));
         }
     }
 
     @SubscribeEvent
     public void onGuiScreenActionPerformed(GuiScreenEvent.ActionPerformedEvent.Post event) {
-        if (event.gui instanceof GuiIngameMenu && event.button.id == 100) {
-            Minecraft.getMinecraft().displayGuiScreen(new GuiMultiplayer(event.gui));
+        if (event.getGui() instanceof GuiIngameMenu && event.getButton().id == 100) {
+            Minecraft.getMinecraft().displayGuiScreen(new GuiMultiplayer(event.getGui()));
         }
     }
 }
